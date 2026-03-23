441d8b75e249c778224f044ae01eb3b730a5ca17
Shun Iwashita <iwashunabc519@gmail.com>
2026-03-23 20:30:36 +0700

    update

--- util/supabase.cjs
+++ util/supabase.cjs
@@ -1,7 +1,5 @@
 const { createClient } = require('@supabase/supabase-js');
 
-// console.log(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
-console.log(process.env.SUPABASE_URL);
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 module.exports = supabase;
