a95d96e487cec8efa8512aeda8317c3a9042dcbe
Shun Iwashita <iwashunabc519@gmail.com>
2026-03-23 19:25:03 +0700

    update

--- app.cjs
+++ app.cjs
@@ -64,6 +64,13 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV === "production") {
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+} else {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
