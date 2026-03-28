98923e3e8355c03eff60c48e67b65475bdd5c736
Shun Iwashita <iwashunabc519@gmail.com>
2026-03-28 20:27:55 +0700

    Added tag for model name

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -12,6 +12,7 @@ import handleEditableForm from './adminEditableHandler';
 import "../scss/loadfonts.scss";
 import "../scss/styles.scss";
 import "../scss/allPartsList.scss";
+import "../scss/productTable.scss";
 
 const scanIconElement = convertSVGToDOMNode(scanIcon, 26, 26);
 const plusIconElement = convertSVGToDOMNode(plusIcon, 32, 32);
